b488bbce31c6dcfcfb6aec507745a40aec3c6457
joao.lima08 <joao.lima@bagy.com.br>
2022-12-14 20:48:08 -0300

    Feat: add project configurations

---
+++ src/views/Credentials/index.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="">
+    <h1>This is an redentials page</h1>
+  </div>
+</template>
---
+++ src/views/Feedbacks/index.vue
@@ -0,0 +1,6 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="">
+    <h1>This is an feedback page</h1>
+  </div>
+</template>
